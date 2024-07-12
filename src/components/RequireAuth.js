f52f3c2f041bf6061686a0a111830619d005b134
Erhahon Imafidon <mafiosco4@gmail.com>
2024-07-12 22:48:00 +0100

    Created the RequireAuth Component to Protect the route for unauthorized or unauthenticated users

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,17 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.js';
+
+// RequireAuth to Protect the routes if user don't have the Authorization or
+// Authentication
+const RequireAuth = () => {
+    const { auth } = useAuth();
+    const location = useLocation();
+
+    return auth?.user ? (
+        <Outlet />
+    ) : (
+        <Navigate to={`/login`} state={{ from: location }} replace />
+    );
+};
+
+export default RequireAuth;
