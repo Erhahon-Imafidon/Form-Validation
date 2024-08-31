91055f97516ed7f0ea1e84e8a5fcf68188c265a4
Erhahon Imafidon <mafiosco4@gmail.com>
2024-08-31 06:46:26 +0100

    Set up the lounge component

--- src/pages/Lounge.jsx
+++ src/pages/Lounge.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Lounge = () => {
-    return <div>Lounge</div>;
+    return (
+        <section>
+            <h1>Lounge</h1>
+            <br />
+            <p>Admins and Editors can hangout here</p>
+            <br />
+            <div className={'grow'}>
+                <Link to={'/'}>Home</Link>
+            </div>
+        </section>
+    );
 };
 
 export default Lounge;
