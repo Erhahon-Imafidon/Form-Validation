0d9eb188f1e56f122182ff0009e4a5f9a411ad05
Erhahon Imafidon <mafiosco4@gmail.com>
2024-10-03 19:18:39 +0100

    Made modifications to the useLocalStorage hook to be more robust for SSR, function

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
 
+// making this hook more robust to use it for any key and value
+const getLocalValue = (key, initValue) => {
+    // for SSR, Next.js or other SSR frameworks
+    if (typeof window === 'undefined') return initValue;
+
+    // if the value is already in the localStorage
+    const localValue = localStorage.getItem(key);
+    if (localValue) return JSON.parse(localValue);
+
+    // if the value is the result of a function
+    if (initValue instanceof Function) return initValue();
+
+    return initValue;
+};
+
 const useLocalStorage = (key, initValue) => {
-    const [value, setValue] = useState(
-        JSON.parse(localStorage.getItem(key)) || initValue
-    );
+    const [value, setValue] = useState(getLocalValue(key, initValue));
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
